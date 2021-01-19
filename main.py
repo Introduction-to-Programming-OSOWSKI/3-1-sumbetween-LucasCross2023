c9216226f8cc7c2f0dda499cc5db381353a8bc88
LucasCross2023 <817750@mystma.org>
2021-01-19 15:53:20 +0000

    sumBetween try 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def sumBetween(x, y):
+    
+    total = 0
+    
+    for i in range (5 + 1, 7):
+        total = total + i
+    
+    return total 
+
+print (sumBetween(5,7))
